fd2c15ac0e03acaa324b86d85b7a60e0bf9a808c
3liana <115217369+3liana@users.noreply.github.com>
2025-04-20 16:42:38 +0200

    Revert "websocket3"
    This reverts commit d36eb191f0cabcdda15c470ce8199e0c742aacdf.

--- src/main/java/org/buaa/project/config/WebSocketConfig.java
+++ src/main/java/org/buaa/project/config/WebSocketConfig.java
@@ -1,13 +1,16 @@
 package org.buaa.project.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-public class WebSocketConfiguration {
-    
+@EnableWebSocket
+public class WebSocketConfig {
+
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
 }
+
