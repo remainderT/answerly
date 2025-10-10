6aaf645fac97fa2928e443e26bf22b9931a694eb
XhesicaFrost <142909332+XhesicaFrost@users.noreply.github.com>
2025-10-10 10:33:54 +0800

    增加找回用户名，重置密码

--- src/main/java/org/buaa/project/dao/mapper/UserMapper.java
+++ src/main/java/org/buaa/project/dao/mapper/UserMapper.java
@@ -2,10 +2,18 @@ package org.buaa.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.buaa.project.dao.entity.UserDO;
-
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 /**
  * 用户持久层
  */
 public interface UserMapper extends BaseMapper<UserDO> {
 
+    // 根据邮箱查询用户名
+    @Select("SELECT username FROM user WHERE mail = #{mail} AND del_flag = 0")
+    String selectUsernameByMail(@Param("mail") String mail);
+
+    // 根据用户名查询邮箱
+    @Select("SELECT mail FROM user WHERE username = #{username} AND del_flag = 0")
+    String selectMailByUsername(@Param("username") String username);
 }
